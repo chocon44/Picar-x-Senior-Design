80da9a30d252fa44bc0943c30601df1874abb0fa
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-08-28 20:01:11 -0400

    Create test_motors.py

---
+++ test_motors.py
@@ -0,0 +1,27 @@
+# This file is created to test inputing different power level to each motor
+# Last updated: 8/28/24
+
+from robot_hat import Motors
+import time
+
+motors = Motors()  # create motors object from class Motors
+
+# identify each motor
+LEFT = 1  
+RIGHT = 2
+
+# Left and right motors have opposite speed to go in the same direction
+
+# go forward
+motors[LEFT].speed(-50)
+motors[RIGHT].speed(50)
+time.sleep(3)
+motors.stop()
+time.sleep(2)
+
+# reverse
+motors[LEFT].speed(50)
+motors[RIGHT}.speed(-50)
+time.sleep(3)
+motors.stop()
+
