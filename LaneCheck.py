c22011724cbc931707a9ec8f83e3a28e67389aa7
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-09-10 13:36:56 -0400

    Create LaneCheck.py

---
+++ LaneCheck.py
@@ -0,0 +1,14 @@
+from picarx import Picarx
+import math
+import time
+
+def LaneCheck():    # this function checks for black line
+   
+    print("Lane check")
+    car = Picarx()
+    
+    # read data from grayscale
+    gm_val_list = car.get_grayscale_data()
+    return gm_val_list
+    
+    
