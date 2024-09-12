21016c03619e6a8b571ce297e980329b67bf7034
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-09-12 14:21:24 -0400

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -32,24 +32,13 @@ def LaneCheck():
     # read data from grayscale
     gm_val_list = car.get_grayscale_data()
         
-    # re-orient the car based on grayscale reading 
-    
     #xxo, xoo
-    if (gm_val_list[0] < 200):    # the black line is on the left of the car, move right 
-        car.forward(0)  # stop the car
-        car.set_dir_servo_angle(20) # rotate servo angle to the right
-        car.forward(power) # go forward for 1 sec
-        time.sleep(0.1)   # CHANGED FROM 0.5
-        reset_turn_servo()  # reset turning angle back to 0
-    #oxx, oox
-    elif (gm_val_list[2] < 200):    # the black line is on the right of the car, move left 
-        car.forward(0)  # stop the car 
-        car.set_dir_servo_angle(-20)    # turn servo to left turn 
-        car.forward(power)  
-        time.sleep(0.1) # pause for half a second then reset servo angle to go straight
+    if (gm_val_list[0] < 300) or (gm_val_list[2] < 200):    # the black line is on the left of the car, move right 
+        # stop turning    
         reset_turn_servo()
-    #xxx, oxo
-    #else:
+        return 1
+    else:
+        return 0
 
 
 def slow_turn_right():
@@ -62,24 +51,19 @@ def slow_turn_right():
     car.set_dir_servo_angle(30) # rotate servo angle to the right 
       
     # # as long as there is no obstacle in the front, continue turning until totalTime is reached
-    # while (ObstacleCheck() == 0):   
-        # car.forward(rightTurnPower)
-        # time.sleep(0.1)
-        # totalTime -= 0.1
-        # if (totalTime == 0):
-            # break       # finish loop when total rightTurnTime has been reached
-    # # if obstacle is detected...
-    # else:
-        # reset_turn_servo() 
-        # car.forward(power)
-        # time.sleep(0.5)
-        # car.forward(0)
+    while (LaneCheck() == 0):   
+         car.forward(rightTurnPower)
+         time.sleep(0.1)
+         totalTime -= 0.1
+         if (totalTime == 0):
+             break       # finish loop when total rightTurnTime has been reached
+    # # if lane is detected...
+    else:
+        reset_turn_servo() 
+        car.forward(power)
+        time.sleep(0.5)
+        car.forward(0)
     
-    while (totalTime != 0):
-        car.forward(rightTurnPower)
-        time.sleep(0.1)
-        LaneCheck()
-        totalTime -= 0.1
         
     
 
@@ -89,29 +73,22 @@ def slow_turn_left():
     global leftTurnTime
     
     totalTime = leftTurnTime
-    car.forward(0)
-    car.set_dir_servo_angle(-30)    # turn servo to left turn  
-    
+    car.forward(0)  # stop the car 
+    car.set_dir_servo_angle(30) # rotate servo angle to the right 
+      
     # # as long as there is no obstacle in the front, continue turning until totalTime is reached
-    # while (ObstacleCheck() == 0):   
-        # car.forward(leftTurnPower)
-        # time.sleep(0.1)
-        # totalTime -= 0.1
-        # if (totalTime == 0):
-            # break       # finish loop when total leftTurnTime has been reached
-    # # if obstacle is detected...
-    # else:
-        # reset_turn_servo() 
-        # car.forward(power)
-        # time.sleep(0.5)
-        # car.forward(0)
-    
-    while (totalTime != 0):
-        car.forward(leftTurnPower)
-        time.sleep(0.1)
-        LaneCheck()
-        totalTime -= 0.1
-    car.forward(0)
+    while (LaneCheck() == 0):   
+         car.forward(leftTurnPower)
+         time.sleep(0.1)
+         totalTime -= 0.1
+         if (totalTime == 0):
+             break       # finish loop when total rightTurnTime has been reached
+    # # if lane is detected...
+    else:
+        reset_turn_servo() 
+        car.forward(power)
+        time.sleep(0.5)
+        car.forward(0)
 
 def main():
     slow_turn_left()
