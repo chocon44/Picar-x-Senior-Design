86f95f805d273df4470a38aff812d8aaf433e9e1
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-09-25 11:17:35 -0400

    Update test_motors.py

--- test_motors.py
+++ test_motors.py
@@ -5,8 +5,8 @@ import time
 
 def main():
     car = Picarx()
-    car.forward(30)
-    time.sleep(1)
+    #car.forward(30)
+    #time.sleep(1)
     car.pivotRight(70)
     time.sleep(1)
     car.stop()
