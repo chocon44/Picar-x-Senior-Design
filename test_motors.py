97ada239757fe92df7ab47c6867be23560ab2b4b
PiplupMantis <noname50842@gmail.com>
2024-09-25 10:26:44 -0400

    9/25

--- test_motors.py
+++ test_motors.py
@@ -1,15 +1,15 @@
 
 from picarx import Picarx
-
-
 import math
-
 import time
 
+def main():
+    car = Picarx()
+    car.forward(30)
+    time.sleep(1)
+    #car.PivotRight(30)
+    #time.sleep(1)
+    car.stop()
+
 car = Picarx()
-car.forward(30)
-time.sleep(1)
-#car.PivotRight(30)
-#time.sleep(1)
-
-
+main()
