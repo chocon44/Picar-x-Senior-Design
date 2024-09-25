5797de52a6e47b2a69a8ce9878d262434b7ac8f4
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-09-25 10:17:10 -0400

    9/25

--- test_motors.py
+++ test_motors.py
@@ -1,53 +1,15 @@
-# This file is created to test inputing different power level to each motor
-# Last updated: 9/10
 
+from pi_folder import Picarx
+=======
 
-from LaneCheck import LaneChecking
-from robot_hat import Motors
+import math
+
 import time
 
 
+car.forward(30)
+time.sleep(1)
+car.PivotRight(30)
+time.sleep(1)
 
 
-motors = Motors()  # create motors object from class Motors
-
-# identify each motor
-LEFT = 1  
-RIGHT = 2
-
-# Left and right motors have opposite speed to go in the same direction
-
-# go forward
-#motors[LEFT].speed(-50)
-#motors[RIGHT].speed(50)
-#time.sleep(0.5)
-#motors.stop()
-#time.sleep(1)
-
-# reverse
-#motors[LEFT].speed(50)
-#motors[RIGHT].speed(-50)
-#time.sleep(0.5)
-#motors.stop()
-
-# turn left 
-def PivotLeft():
-    global motors
-    motors[LEFT].speed(70)
-    motors[RIGHT].speed(70)
-    time.sleep(1.6)     # turn 90 degrees
-    motors.stop()
-
-# turn right
-def PivotRight():
-    global motors
-    motors[LEFT].speed(-70)
-    motors[RIGHT].speed(-70)
-    time.sleep(1.6)
-    motors.stop()
-
-
-
-PivotLeft()
-list = LaneChecking()
-print(list[0])
