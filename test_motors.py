7359cb2a95a6383b534771d4bc676fb13055898b
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-09-25 12:29:34 -0400

    m

--- test_motors.py
+++ test_motors.py
@@ -6,9 +6,10 @@ import time
 
 def test_ultra():
     car = Picarx()
-    car.set_cam_ultra_angle(90)
-    time.sleep(1)
-    car.set_cam_ultra_angle(-90)
+    car.set_cam_pan_angle(x_angle)
+    #car.set_cam_ultra_angle(90)
+    #time.sleep(1)
+    #car.set_cam_ultra_angle(-90)
 
 def main():
     car = Picarx()
