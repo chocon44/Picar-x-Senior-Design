7f92cab6e1a3839a225c7a0dcb2cdc6504d86f87
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-09-27 14:25:09 -0400

    Update 20x20_Astar.py

--- 20x20_Astar.py
+++ 20x20_Astar.py
@@ -1,1 +1,25 @@
 # Shortest path algorithm for 1 car -- Pico or Picarx
+# Resource : Slide 12 #29
+
+# Get starting coordinate between (0,0) and (20,20)
+def GetStartingPoint():
+
+# Get ending coordinate between (0,0) and (20,20)
+def GetEndingPoint():
+
+# Calculature Heuristic value for each square
+def Heuristic():
+
+
+def GFunction():
+
+
+def HFunction():
+
+  
+  
+
+def main():
+
+
+
