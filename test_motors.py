5de99e2e08c5136adbcb9666011a4d755d1bbdd6
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-10-16 17:05:16 -0400

    Added pan camera angle testing

--- test_motors.py
+++ test_motors.py
@@ -26,9 +26,24 @@ def ultra():
         angle -= 10
     car.set_cam_ultra_angle(0)
 
+def pan():
+    car.set_cam_pan_angle(0)
+    time.sleep(1)
+    angle = -90
+    while (angle <= 90):
+        car.set_cam_pan_angle(angle)
+        time.sleep(0.2)
+        angle += 10
+    while (angle >= -90):
+        car.set_cam_pan_angle(angle)
+        time.sleep(0.2)
+        angle -= 10
+    car.set_cam_pan_angle(0)
+
 def main():
     #motors()
-    ultra()
+    #ultra()
+    pan()
     car.stop()
 
 main()
