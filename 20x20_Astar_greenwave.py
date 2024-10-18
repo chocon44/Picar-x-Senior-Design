a81fbcfc378cfb63086b21d2cfb52336dcf97b40
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-10-18 14:54:00 -0400

    c

--- 20x20_Astar_greenwave.py
+++ 20x20_Astar_greenwave.py
@@ -236,7 +236,7 @@ def ObstacleAhead():
         obsAngle = angle    # note the angle obstacle is detected
         time.sleep(2)       # wait 2 seconds before checking again 
         ObstacleAhead()     # repeat this function until the obstacle is cleared
-    else:   # when no close obstacle is detected, do nothing
+    else:   # when no close obstacle is detected
         return
     
 # flags to indicate orientation 
