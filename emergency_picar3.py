7d94b5d2e132043d494f70d7eff4a945cbde8da8
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-10-30 18:16:42 -0400

    m

--- emergency_picar3.py
+++ emergency_picar3.py
@@ -38,7 +38,7 @@ def main():
 
     #speed = 50
     car.set_motor_speed(1, 90) #right motor
-    car.set_motor_speed(2,-1*30)   # left motor
+    car.set_motor_speed(2,-1*20)   # left motor
     time.sleep(2)
 
     time.sleep(3)
