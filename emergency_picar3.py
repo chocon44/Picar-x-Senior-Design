a6b8679431611495c4a21d42f39cb4efc33cb35a
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-10-30 18:00:26 -0400

    10/30

---
+++ emergency_picar3.py
@@ -0,0 +1,45 @@
+from picarx import Picarx 
+import math 
+import time
+import numpy as np
+import heapq
+from typing import List, Tuple
+import pyrebase
+#from vilib import Vilib
+from robot_hat import Motors
+
+
+#-------- Firebase -----------#
+
+config = {
+    'apiKey': "AIzaSyBL9UezOELm7LsBLvfZdlJDkhCTZQAKlx4",
+      'authDomain': "jetbot-ece-493.firebaseapp.com",
+      'projectId': "jetbot-ece-493",
+      'storageBucket': "jetbot-ece-493.appspot.com",
+      'messagingSenderId': "906874678062",
+      'appId': "1:906874678062:web:6c50c28e26fbb59c0ca8bf",
+      'databaseURL': "https://jetbot-ece-493-default-rtdb.firebaseio.com/"
+    }
+
+
+
+
+car = Picarx() 
+
+def main():
+    # Initialize Firebase
+    firebase = pyrebase.initialize_app(config)
+    database = firebase.database()
+
+    data = {
+        "Presence": 1}
+        
+    database.child("Emergency vehicle").set(data)
+
+    speed = 30
+    car.set_motor_speed(1, speed)
+    car.set_motor_speed(2,-1*speed)
+    time.sleep(3)
+    car.stop() 
+
+main()
