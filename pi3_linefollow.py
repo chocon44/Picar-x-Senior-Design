4cc7ad94408523c3430394e08260f2921c90945b
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-10-31 13:41:34 -0400

    a

--- pi3_linefollow.py
+++ pi3_linefollow.py
@@ -7,7 +7,7 @@ px = Picarx()
 
 # Please run ./calibration/grayscale_calibration.py to Auto calibrate grayscale values
 # or manual modify reference value by follow code
-px.set_line_reference([0, 1400, 0])
+#px.set_line_reference([0, 1400, 0])
 
 current_state = None
 px_power = 10
@@ -32,24 +32,16 @@ def outHandle():
     sleep(0.001)
 
 def get_status(val_list):
-    _state = px.get_line_status(val_list)  # [bool, bool, bool], 0 means line, 1 means background
+   _state = px.get_line_status(val_list)  # [bool, bool, bool], 0 means line, 1 means background
     if _state == [0, 0, 0]:
+        return 'stop'
+    elif _state[1] == 1:
         return 'forward'
-    elif _state == [1,1,1]:
-        return 'stop'
-    elif _state == [1,1,0]:
-        return 'left'
-    elif _state == [0,1,1]:
+    elif _state[0] == 1:
         return 'right'
-    else:
-        return 'stop'
+    elif _state[2] == 1:
+        return 'left'
 
-    #elif _state[1] == 1:
-    #    return 'forward'
-    #elif _state[0] == 1:
-    #    return 'right'
-    #elif _state[2] == 1:
-    #    return 'left'
 
 if __name__=='__main__':
     try:
