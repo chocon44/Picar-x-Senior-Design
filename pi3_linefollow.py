3eec51b115bf42b7dbd2dc5cc11b39d952012e76
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-10-31 16:01:49 -0400

    a

--- pi3_linefollow.py
+++ pi3_linefollow.py
@@ -8,65 +8,29 @@ px = Picarx()
 
 # Please run ./calibration/grayscale_calibration.py to Auto calibrate grayscale values
 # or manual modify reference value by follow code
-px.set_line_reference([1400, 1400, 1400])
+#px.set_line_reference([1400, 1400, 1400])
 
-current_state = None
-px_power = 10
-offset = 20
-last_state = "stop"
 
-def outHandle():
-    global last_state, current_state
-    if last_state == 'left':
-        px.set_dir_servo_angle(-30)
-        px.backward(10)
-    elif last_state == 'right':
-        px.set_dir_servo_angle(30)
-        px.backward(10)
-    while True:
+def main():
+    max_time = time.time() + 7
+    while time.time() < max_time:
         gm_val_list = px.get_grayscale_data()
-        gm_state = get_status(gm_val_list)
-        print("outHandle gm_val_list: %s, %s"%(gm_val_list, gm_state))
-        currentSta = gm_state
-        if currentSta != last_state:
-            break
-    sleep(0.001)
+        if gm_val_list[1] > 1000:
+            px.set_dir_servo_angle(0)
+            px.forward(px_power)
+        
+        elif (gm_val_list[0] > 1000) and (gm_val_list[1] > 1000) and (gm_val_list[2] > 1000):
+            px.set_dir_servo_angle(0)
+            px.forward(px_power)
+        elif gm_val_list[0] > 1000:   # line is on the left -- move right
+            px.set_dir_servo_angle(-offset)
+            px.forward(px_power)
+        elif gm_val_list[2] > 1000:   # line is on the right -- move left
+            px.set_dir_servo_angle(offset)
+            px.forward(px_power)
+        else:
+            px.stop()
+    px.stop()
 
-def get_status(val_list):
-    _state = px.get_line_status(val_list)  # [bool, bool, bool], 0 means line, 1 means background
-    if _state == [0, 0, 0]:
-        return 'stop'
-    elif _state[1] == 1:
-        return 'forward'
-    elif _state[0] == 1:
-        return 'right'
-    elif _state[2] == 1:
-        return 'left'
+main()
 
-if __name__=='__main__':
-    try:
-        max_time = time.time() + 7
-        while time.time() < max_time:
-            gm_val_list = px.get_grayscale_data()
-            if gm_val_list[1] > 1000:
-                px.set_dir_servo_angle(0)
-                px.forward(px_power)
-            
-            elif (gm_val_list[0] > 1000) and (gm_val_list[1] > 1000) and (gm_val_list[2] > 1000):
-                px.set_dir_servo_angle(0)
-                px.forward(px_power)
-            elif gm_val_list[0] > 1000:   # line is on the left -- move right
-                px.set_dir_servo_angle(-offset)
-                px.forward(px_power)
-            elif gm_val_list[2] > 1000:   # line is on the right -- move left
-                px.set_dir_servo_angle(offset)
-                px.forward(px_power)
-            else:
-                px.stop()
-        px.stop()
-
-            
-    finally:
-        px.stop()
-        print("stop and exit")
-        sleep(0.1)
