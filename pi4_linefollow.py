3a77537308e29f613c8259c2eeba0db94a4bfa0d
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-10-31 16:28:50 -0400

    1

--- pi4_linefollow.py
+++ pi4_linefollow.py
@@ -13,12 +13,27 @@ ref = 600
 #px.set_line_reference([1400, 1400, 1400])
 
 def Forward():
-    px.set_dir_servo_angle(0)
-    px.forward(px_power)
-    time.sleep(1)
+    max_time = time.time() + 1  # set max time 
+    while (time.time() < max_time):
+        gm_val_list = px.get_grayscale_data()
+        if gm_val_list[1] > ref:
+            px.set_dir_servo_angle(0)
+            px.forward(px_power)
+
+        elif (gm_val_list[0] > ref) and (gm_val_list[1] > ref) and (gm_val_list[2] > ref):
+            px.set_dir_servo_angle(0)
+            px.forward(px_power)
+        elif gm_val_list[0] > ref:   # line is on the left -- move right
+            px.set_dir_servo_angle(-offset)
+            px.forward(px_power)
+        elif gm_val_list[2] > ref:   # line is on the right -- move left
+            px.set_dir_servo_angle(offset)
+            px.forward(px_power)
+        else:
+            px.stop()
     px.stop()
     px.set_dir_servo_angle(0)
-    return
+    
 
 def main():
     max_time = time.time() + 10
