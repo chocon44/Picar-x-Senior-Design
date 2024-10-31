957577cf1e3d3086f225cbee72d843c21bed4bff
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-10-31 13:39:34 -0400

    a

--- pi3_linefollow.py
+++ pi3_linefollow.py
@@ -34,7 +34,7 @@ def outHandle():
 def get_status(val_list):
     _state = px.get_line_status(val_list)  # [bool, bool, bool], 0 means line, 1 means background
     if _state == [0, 0, 0]:
-        return 'stop'
+        return 'forward'
     elif _state == [1,1,1]:
         return 'stop'
     elif _state == [1,0,0]:
