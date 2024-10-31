7b5ab231ee87ed442681a438072b1e5e9533abeb
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-10-31 13:32:06 -0400

    a

--- pi3_linefollow.py
+++ pi3_linefollow.py
@@ -1,13 +1,12 @@
 from picarx import Picarx
 from time import sleep
-import time
 
 px = Picarx()
 # px = Picarx(grayscale_pins=['A0', 'A1', 'A2'])
 
 # Please run ./calibration/grayscale_calibration.py to Auto calibrate grayscale values
 # or manual modify reference value by follow code
-#px.set_line_reference([0, 1300, 0])
+# px.set_line_reference([1400, 1400, 1400])
 
 current_state = None
 px_power = 10
@@ -33,28 +32,18 @@ def outHandle():
 
 def get_status(val_list):
     _state = px.get_line_status(val_list)  # [bool, bool, bool], 0 means line, 1 means background
-    # testing
-    #for i in _state:
-    #    print (i, end=" ")
-    #print()
-    if _state == [0, 0, 0]: # all three get the referenced values -- forward
-        #return 'stop'
+    if _state == [0, 0, 0]:
+        return 'stop'
+    elif _state[1] == 1:
         return 'forward'
-    elif _state == [0,0,1]: # left and middle get the line -- move slight left 
-        return 'slight left'
-    elif _state == [1,0,0]: # right and middle  get the line -- move slight right
-        return 'slight right'
-    elif _state == [1,0,1]: # middle gets the line -- forward
-        return 'forward'
-    elif _state == [1,1,0]: # only right gets the line -- left
-        return 'left'
-    elif _state == [0,1,1]: # only left gets the line -- right
+    elif _state[0] == 1:
         return 'right'
+    elif _state[2] == 1:
+        return 'left'
 
 if __name__=='__main__':
     try:
-        t_end = time.time() + 7     # 5 sec max
-        while (time.time()< t_end):
+        while True:
             gm_val_list = px.get_grayscale_data()
             gm_state = get_status(gm_val_list)
             print("gm_val_list: %s, %s"%(gm_val_list, gm_state))
@@ -71,18 +60,9 @@ if __name__=='__main__':
             elif gm_state == 'right':
                 px.set_dir_servo_angle(-offset)
                 px.forward(px_power)
-            elif gm_state == 'slight right':
-                px.set_dir_servo_angle(-offset+5)
-                px.forward(px_power)
-            elif gm_state == 'slight left':
-                px.set_dir_servo_angle(offset-5)
-                px.forward(px_power)
-                
-                
             else:
                 outHandle()
-        px.stop()
     finally:
         px.stop()
         print("stop and exit")
-        
+        sleep(0.1)
