8e7259c6e07074955f4c0df94a073e4fd02a9695
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-10-31 12:59:31 -0400

    a

--- pi3_linefollow.py
+++ pi3_linefollow.py
@@ -4,7 +4,7 @@ import time
 
 px = Picarx()
 # px = Picarx(grayscale_pins=['A0', 'A1', 'A2'])
-#px.set_line_reference([1400, 1400, 1400])
+px.set_line_reference([500, 1400, 500])
 
 current_state = None
 px_power = 10
