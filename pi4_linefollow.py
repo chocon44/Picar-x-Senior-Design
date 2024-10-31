176ce1765a394d40cf5ec6db161d8f4c9e976d7f
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-10-31 17:06:10 -0400

    m

--- pi4_linefollow.py
+++ pi4_linefollow.py
@@ -16,7 +16,7 @@ ref = 600
 
 def go_forward():
     Vilib.camera_close()
-    max_time = time.time() + 1.3  # set max time 
+    max_time = time.time() + 1.2  # set max time 
     # go straight
     while (time.time() < max_time):
         gm_val_list = px.get_grayscale_data()
@@ -40,7 +40,7 @@ def go_forward():
 
 def go_forward_again():
     Vilib.camera_close()
-    max_time = time.time() + 1.5  # set max time 
+    max_time = time.time() + 1.4  # set max time 
     # go straight
     while (time.time() < max_time):
         gm_val_list = px.get_grayscale_data()
@@ -92,7 +92,7 @@ def Forward():
     RedLight()
     ObstacleSweep()
     go_forward_again()
-    
+
     # start turningg here, check for light and obstacle
     #RedLight()
     #
