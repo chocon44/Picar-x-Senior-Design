d69ce561b4ac044116808109eff68684d21078c9
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-11-01 10:46:04 -0400

    path defined

--- pi4_linefollow.py
+++ pi4_linefollow.py
@@ -1,9 +1,12 @@
 # This code is done for picar 4
+# Go straight for 1 blocks, then turn left
+# Car will start at intersection 
 
 from picarx import Picarx
 from time import sleep
 import time
 from vilib import Vilib
+import math
 
 px = Picarx()
 px_power = 30
@@ -14,11 +17,13 @@ ref = 600
 #px.set_line_reference([1400, 1400, 1400])
 
 
-def go_forward():
+# x is the time travel forward 
+def go_forward(x):
     Vilib.camera_close()
-    max_time = time.time() + 1.25  # set max time 
-    # go straight
+    max_time = time.time() + x
+    #max_time = time.time() + 1.45       # time to travel 1 block, starting from intersection 
     while (time.time() < max_time):
+        ObstacleAhead()     # scan for obstacle ahead
         gm_val_list = px.get_grayscale_data()
         if gm_val_list[1] > ref:
             px.set_dir_servo_angle(0)
@@ -36,92 +41,27 @@ def go_forward():
         else:
             px.stop()
     px.stop()
-    return
+    return 
 
-def go_forward_again():
+
+def go_right():
     Vilib.camera_close()
-    max_time = time.time() + 1.45  # set max time 
-    # go straight
-    while (time.time() < max_time):
+    
+    time_max = time.time() + 3      # max turning time is 2 sec 
+    while (time.time() <= time_max):
+        px.set_dir_servo_angle(-30)
+        px.forward(10)
         gm_val_list = px.get_grayscale_data()
-        if gm_val_list[1] > ref:
-            px.set_dir_servo_angle(0)
-            px.forward(px_power)
-
-        elif (gm_val_list[0] > ref) and (gm_val_list[1] > ref) and (gm_val_list[2] > ref):
-            px.set_dir_servo_angle(0)
-            px.forward(px_power)
-        elif gm_val_list[0] > ref:   # line is on the left -- move right
-            px.set_dir_servo_angle(-offset)
-            px.forward(px_power)
-        elif gm_val_list[2] > ref:   # line is on the right -- move left
-            px.set_dir_servo_angle(offset)
-            px.forward(px_power)
-        else:
-            px.stop()
-    px.stop()
-    return
-
-def go_left():
-    
-def Forward():
-
-    def RedLight():  
-        Vilib.camera_start()
-        #Vilib.display()        # toggle display on when needed
-        Vilib.color_detect("red")
-        # red signal detect
-        if Vilib.detect_obj_parameter['color_n']!=0:    # if red is detected
-            print("Red light detected")
-            px.stop()      # stop the car immediately
-            Vilib.camera_close()
-            
-            # TESTING PURPOSE push redlight result to firebase
-            # data = {
-            # "Redlight detected": 1}
-            # database.child("Picarx4").child("Red light detected").set(data)
-            
-            px.stop()
-            time.sleep(1)
-            RedLight()      # check red light again
-
-    go_forward()
-    px.stop()
-    time.sleep(1)
-
-    # check for traffic light before moving to the next block 
-    RedLight()
-    ObstacleSweep()
-    go_forward_again()
-
-    # start turningg here, check for light and obstacle
-    #RedLight()
-    #
-    
-    #px.left(50)
-    #time.sleep(1.3)
-
-    # go forward for 1 more block
-    #go_forward(1.5)
+        # check if grayscale is getting the line 
+        for val in gm_val_list:
+            if val > ref:   # if one of the sensors caught the line break the turnin loop 
+                break
+        break 
+    return 
 
 
-    px.stop()
-    px.set_dir_servo_angle(0)
-
-
-def ObstacleAhead():
-    Vilib.camera_close()
-    danger = 10
-    dist = round(px.ultrasonic.read(),2)
-    Vilib.camera_close()
-    if (dist > 0) and (dist <= danger):      # if obstacle is detected closely
-        px.stop()          # stop the px 
-        print("Obstacle detected at: ", dist)
-        time.sleep(2)       # wait 2 seconds before checking again 
-        ObstacleAhead()     # repeat this function until the obstacle is cleared
-    return
-
-
+# this function is used to detected obstacles at intersections by sweeping ultrasonic sensor 
+# 100 degrees left to right 
 def ObstacleSweep():
     Vilib.camera_close()
     px.set_cam_pan_angle(0)    # reset pan servo angle 
@@ -136,7 +76,7 @@ def ObstacleSweep():
         # read ultrasonic sensor value 
         dist = round(px.ultrasonic.read(),2)
         if (dist > 0) and (dist <= danger):      # if obstacle is detected closely
-            px.stop()          # stop the car 
+            px.stop()          # stop the px 
             print("Obstacle detected at: ", dist)
             obsAngle = angle    # note the angle obstacle is detected
             time.sleep(waitTime)       # wait 2 seconds before checking again 
@@ -151,7 +91,7 @@ def ObstacleSweep():
         # read ultrasonic sensor value 
         dist = round(px.ultrasonic.read(),2)
         if (dist > 0) and (dist <= danger):        # if obstacle is detected closely
-            px.stop()          # stop the car 
+            px.stop()          # stop the px 
             print("Obstacle detected at: ", dist)
             obsAngle = angle    # note the angle obstacle is detected
             time.sleep(waitTime)       # wait 2 seconds before checking again 
@@ -161,29 +101,72 @@ def ObstacleSweep():
         angle -= 10
     px.set_cam_pan_angle(0)        # reset pan servo angle at the end    
     return
+    
+    
+# This function is used to detect other cars when going straight
+def ObstacleAhead():
+    Vilib.camera_close()
+    danger = 10
+    dist = round(car.ultrasonic.read(),2)
+    Vilib.camera_close()
+    if (dist > 0) and (dist <= danger):      # if obstacle is detected closely
+        px.stop()          # stop the car 
+        print("Obstacle detected at: ", dist)
+        time.sleep(1)       # wait 1 sec before checking again 
+        ObstacleAhead()     # repeat this function until the obstacle is cleared
+    return
+    
+    
+# This function starts camera to check for red light
+def RedLight():  
+    Vilib.camera_start()
+    #Vilib.display()        # toggle display on when needed
+    Vilib.color_detect("red")
+    # red signal detect
+    if Vilib.detect_obj_parameter['color_n']!=0:    # if red is detected
+        print("Red light detected")
+        px.stop()      # stop the car immediately
+        time.sleep(0.5)   
+        RedLight()      # check red light again 
+    else:        # if red is not detected -- green or yellow
+        Vilib.camera_close()
+        return    
+
+
 
 
 
 def main():
-    max_time = time.time() + 10
-    while time.time() < max_time:
-        gm_val_list = px.get_grayscale_data()
-        if gm_val_list[1] > ref:
-            px.set_dir_servo_angle(0)
-            px.forward(px_power)
-
-        elif (gm_val_list[0] > ref) and (gm_val_list[1] > ref) and (gm_val_list[2] > ref):
-            px.set_dir_servo_angle(0)
-            px.forward(px_power)
-        elif gm_val_list[0] > ref:   # line is on the left -- move right
-            px.set_dir_servo_angle(-offset)
-            px.forward(px_power)
-        elif gm_val_list[2] > ref:   # line is on the right -- move left
-            px.set_dir_servo_angle(offset)
-            px.forward(px_power)
-        else:
-            px.stop()
-    px.stop()
-    px.set_dir_servo_angle(0)
-
-Forward()
+    Vilib.camera_close()
+    
+    
+    # car will start at intersection 
+    # check for light 
+    RedLight()
+    
+    # sweep for obstacle 
+    ObstacleSweep()
+    
+    # go forward while checking obstacle ahead (1 block) 
+    go_forward(1.45)
+    
+    # prepare to turn right 
+    RedLight()           # check for light 
+    
+    ObstacleSweep()     # sweep for obstacle 
+    # rotate steering servo while going forward in small steps until 1 sensor meets the white line 
+    go_right()
+    
+    # let the car go forward, following that white line (3 blocks)
+    go_forward(1.2)    # go forward for a short bit to meet the intersection 
+    RedLight()          # check for light 
+    ObstacleSweep()    # sweep for obstacle 
+    
+    go_forward(1.45)    # go forward for a short bit to meet the intersection 
+    RedLight()          # check for light 
+    ObstacleSweep()    # sweep for obstacle 
+    
+    go_forward(1.45)    # go forward for a short bit to meet the intersection 
+    RedLight()          # check for light 
+    ObstacleSweep()    # sweep for obstacle 
+        
