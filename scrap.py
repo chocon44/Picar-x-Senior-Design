acb942db21cfd6bf6ddff5ec5790e35e019e6819
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-11-01 16:00:08 -0400

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -14,17 +14,7 @@ def main():
         while True:
             distance = round(px.ultrasonic.read(), 2)
             print("distance: ",distance)
-            if distance >= SafeDistance:
-                px.set_dir_servo_angle(0)
-                px.forward(POWER)
-            elif distance >= DangerDistance:
-                px.set_dir_servo_angle(30)
-                px.forward(POWER)
-                time.sleep(0.1)
-            else:
-                px.set_dir_servo_angle(-30)
-                px.backward(POWER)
-                time.sleep(0.5)
+            
 
     finally:
         px.forward(0)
