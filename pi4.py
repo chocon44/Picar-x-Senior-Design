4f088cb0f7384c1ec24ea73ab86244cc8efbf7e6
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-11-01 15:42:48 -0400

    60

--- pi4.py
+++ pi4.py
@@ -9,7 +9,7 @@ from vilib import Vilib
 import math
 
 px = Picarx()
-px_power = 50
+px_power = 60
 offset = 20
 ref = 500
 short = 0.8
@@ -168,9 +168,10 @@ def main():
     try: 
         RedLight()
         go_forward()
+        
         #RedLight()
         #go_forward_short()
-        px.stop()
+       
         #RedLight()
         #ObstacleSweep()
 
