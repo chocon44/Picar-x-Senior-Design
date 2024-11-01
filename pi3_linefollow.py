8da44d12f4522e333d8c17762a96a1052038c00f
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-11-01 10:48:59 -0400

    firebase added

--- pi3_linefollow.py
+++ pi3_linefollow.py
@@ -3,6 +3,23 @@
 from picarx import Picarx
 from time import sleep
 import time
+import pyrebase 
+
+#-------- Firebase -----------#
+
+config = {
+    'apiKey': "AIzaSyBL9UezOELm7LsBLvfZdlJDkhCTZQAKlx4",
+      'authDomain': "jetbot-ece-493.firebaseapp.com",
+      'projectId': "jetbot-ece-493",
+      'storageBucket': "jetbot-ece-493.appspot.com",
+      'messagingSenderId': "906874678062",
+      'appId': "1:906874678062:web:6c50c28e26fbb59c0ca8bf",
+      'databaseURL': "https://jetbot-ece-493-default-rtdb.firebaseio.com/"
+    }
+
+firebase = pyrebase.initialize_app(config)
+database = firebase.database()
+
 
 px = Picarx()
 px_power = 30
@@ -13,6 +30,12 @@ ref = 600
 #px.set_line_reference([1400, 1400, 1400])
 
 def main():
+    data = {
+        "Presence": 1}
+        
+    database.child("Emergency vehicle").set(data)
+    time.sleep(3)    # notify for 3 sec before emergency car starts moving
+    
     max_time = time.time() + 10
     while time.time() < max_time:
         gm_val_list = px.get_grayscale_data()
@@ -32,5 +55,11 @@ def main():
         else:
             px.stop()
     px.stop()
+    data = {
+        "Presence": 0}
+        
+    database.child("Emergency vehicle").set(data)
+   
+
 
 main()
