b0b6a7478319741ac710a5cc36b8ffe1aec41c92
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-11-01 15:15:08 -0400

    m

--- pi4.py
+++ pi4.py
@@ -168,18 +168,18 @@ def main():
     try:
         Vilib.camera_close()
         
-        # go_forward_short()
+        go_forward_short()
         
         # # car will start at intersection 
         
-        # RedLight()  # check for light 
-        # ObstacleSweep() # sweep for obstacle 
+        RedLight()  # check for light 
+        ObstacleSweep() # sweep for obstacle 
         
-        # go_forward()    # go forward while checking obstacle ahead (1 block) 
+        go_forward()    # go forward while checking obstacle ahead (1 block) 
         
         # # # prepare to turn right 
-        # RedLight()           # check for light 
-        # ObstacleSweep()     # sweep for obstacle 
+        RedLight()           # check for light 
+        ObstacleSweep()     # sweep for obstacle 
 
         # the above parameters passed
 
@@ -192,9 +192,9 @@ def main():
         
         
         # # let the car go forward, following that white line (3 blocks)
-        #go_forward_short()    # go forward for a short bit to meet the intersection 
-        # RedLight()          # check for light 
-        # ObstacleSweep()    # sweep for obstacle 
+        go_forward_short()    # go forward for a short bit to meet the intersection 
+        RedLight()          # check for light 
+        ObstacleSweep()    # sweep for obstacle 
         
         # go_forward()    # go forward for a short bit to meet the intersection 
         # RedLight()          # check for light 
