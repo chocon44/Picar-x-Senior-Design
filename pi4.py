631856edc4cba27a6668584bbb666984612edc06
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-11-01 15:38:04 -0400

    a

--- pi4.py
+++ pi4.py
@@ -13,7 +13,7 @@ px_power = 50
 offset = 20
 ref = 500
 short = 0.8
-long = 3     # time to travel 1 block 
+long = 3.3     # time to travel 1 block 
 
 # px = Picarx(grayscale_pins=['A0', 'A1', 'A2'])
 
