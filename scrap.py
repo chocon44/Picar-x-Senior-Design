42763149dcc6a92c86a8973fab9cec2fb1f82e37
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-11-02 12:35:42 -0400

    a

--- scrap.py
+++ scrap.py
@@ -53,8 +53,8 @@ if __name__ == "__main__":
             if key in('wsadikjlgh'):        # added g and h
                 if 'w' == key:          # go forward 
                     px.set_dir_servo_angle(0)
-                    #px.forward(power)
-                    px.set_motor_speed(1, power)
+                    px.forward(power)
+                    #px.set_motor_speed(1, power)
                     #px.set_motor_speed(2, -1*power)  
                    
 
