93cfa332d23347fc6c3206a6122f202821a63f6b
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-11-02 12:22:27 -0400

    a

--- scrap.py
+++ scrap.py
@@ -48,8 +48,27 @@ if __name__ == "__main__":
             key = key.lower()
             if key in('wsadikjlgh'):        # added g and h
                 if 'w' == key:          # go forward 
-                    px.set_dir_servo_angle(0)
-                    px.forward(power)
+                    #px.set_dir_servo_angle(0)
+                    #px.forward(power)
+                    max_time = time.time() + 1
+                    while time.time() < max_time:
+                        gm_val_list = px.get_grayscale_data()
+                        if gm_val_list[1] > ref:
+                            px.set_dir_servo_angle(0)
+                            px.forward(px_power)
+
+                        elif (gm_val_list[0] > ref) and (gm_val_list[1] > ref) and (gm_val_list[2] > ref):
+                            px.set_dir_servo_angle(0)
+                            px.forward(px_power)
+                        elif gm_val_list[0] > ref:   # line is on the left -- move right
+                            px.set_dir_servo_angle(-offset)
+                            px.forward(px_power)
+                        elif gm_val_list[2] > ref:   # line is on the right -- move left
+                            px.set_dir_servo_angle(offset)
+                            px.forward(px_power)
+                        else:
+                            px.stop()
+
                 elif 's' == key:        # go backward
                     px.set_dir_servo_angle(0)
                     px.backward(power)
