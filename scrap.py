56d4c6253559195bde73b940a3df372bc8432ebc
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-11-02 12:38:58 -0400

    +20 to motor 1

--- scrap.py
+++ scrap.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
                 if 'w' == key:          # go forward 
                     px.set_dir_servo_angle(0)
                     #px.forward(power)
-                    px.set_motor_speed(1, power)
+                    px.set_motor_speed(1, power+20)
                     px.set_motor_speed(2, -1*power)  
                    
 
