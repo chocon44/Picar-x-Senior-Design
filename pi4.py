372cc00d5ebc89473cd1bd7fe87c0db664f4a40a
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-11-04 17:49:23 -0500

    a

--- pi4.py
+++ pi4.py
@@ -171,13 +171,13 @@ def main():
         #time.sleep(1)
 
         px.left(30)
-        time.sleep(1.2)
+        time.sleep(1.5)
         
 
         
         go_forward()
 
-        
+
         #px.forward(px_power)
         #time.sleep(1)
 
