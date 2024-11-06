1873a88ef8188e80569edc5fd169cd035d13df5f
PiplupMantis <159501161+PiplupMantis@users.noreply.github.com>
2024-11-06 18:07:49 -0500

    a

--- pi4.py
+++ pi4.py
@@ -17,7 +17,7 @@ px_power2 = 35    # used for speed up only
 offset = 20
 ref = 500
 short = 0.7    # used for speed up only
-long = 2.6     # time to travel 1 block 
+long = 2.8     # time to travel 1 block 
 bit = 2.7
 
 # px = Picarx(grayscale_pins=['A0', 'A1', 'A2'])
